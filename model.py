a7113f76598552d7dc821e638c5547f7a5abb159
MENSTRUE <menstruesalvation@gmail.com>
2025-09-20 21:21:19 +0700

    evaluasi dan simpan

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import joblib
+import pickle
 
 # import model yang akan digunakan
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import GradientBoostingRegressor
 
-from data_splitting import y_train, y_test
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 # persiapan data
 try:
@@ -47,7 +49,7 @@ print("\n" + "-"*50 + "\n")
 
 # model linear regression
 print("--- Melatih Model 2: Linear Regression ---")
-LR = LinearRegression().fix(x_train, y_train)
+LR = LinearRegression().fit(x_train, y_train)
 print("linear regression telah di latih")
 print("\n" + "-"*50 + "\n")
 
@@ -58,3 +60,59 @@ print("Gradient Boosting Regressor telah dilatih.")
 print("\n" + "="*50 + "\n")
 
 print("Semua model telah berhasil dilatih menggunakan data training.")
+
+# evaluasi model
+print("memuali evaluasi model")
+
+# evaluasi (Lars)
+pred_lars = lars.predict(x_test)
+mae_lars = mean_absolute_error(y_test, pred_lars)
+mse_lars = mean_squared_error(y_test, pred_lars)
+r2_lars = r2_score(y_test, pred_lars)
+
+# membuat dictionary
+data = {
+    'MAE': [mae_lars],
+    'MSE': [mse_lars],
+    "R2": [r2_lars]
+}
+
+# konversi distionary menjadi dataframe
+df_results = pd.DataFrame(data, index=['Lars'])
+print("\n hasil evaluasi lars")
+print(df_results)
+
+# evaluasi (LR)
+pred_LR = LR.predict(x_test)
+mae_LR = mean_absolute_error(y_test, pred_LR)
+mse_LR = mean_squared_error(y_test, pred_LR)
+r2_LR = r2_score(y_test, pred_LR)
+
+df_results.loc['Linear Regression'] = [mae_LR, mse_LR, r2_LR]
+print("\nPerbandingan Hasil Evaluasi Setelah Menambah Linear Regression:")
+print(df_results)
+
+# evaluasi (GBR)
+pred_GBR = GBR.predict(x_test)
+mae_GBR = mean_absolute_error(y_test, pred_GBR)
+mse_GBR = mean_squared_error(y_test, pred_GBR)
+r2_GBR = r2_score(y_test, pred_GBR)
+
+# menambahkan hasil evaluasi ke dataframe
+df_results.loc['GradientBoostingRegressor'] = [mae_GBR, mse_GBR, r2_GBR]
+print("hasil evaluasi terakhir")
+print(df_results)
+print("\n" + "="*50 + "\n")
+print("Proses evaluasi model selesai.")
+
+# menyimpan model
+print("Menyimpan Model Terbaik (Gradient Boosting Regressor)")
+
+# menggunakan joblib
+joblib.dump(GBR, 'gbr_model.joblib')
+print("model berhasil disimpan (gbr_model.joblib)")
+
+# menggunkaan pickle
+with open('gbr_model.pkl', 'wb')as file:
+    pickle.dump(GBR, file)
+print("model berhasil disimpan (pickle)")
